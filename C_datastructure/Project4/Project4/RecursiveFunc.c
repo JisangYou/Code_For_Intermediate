a6fd4cf232b6ea027ceff0398a0d3c9caead4acc
youjisang <jisangman89@gmail.com>
2019-04-09 09:39:39 +0900

    Recursive DS

--- C_datastructure/Project4/Project4/RecursiveFunc.c
+++ C_datastructure/Project4/Project4/RecursiveFunc.c
@@ -9,8 +9,25 @@ void Recursive(int num)
 	Recursive(num - 1);
 }
 
+int Factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
+
 int main(void)
 {
+
+	printf("1! = %d \n", Factorial(1));
+	printf("2! = %d \n", Factorial(2));
+	printf("3! = %d \n", Factorial(3));
+	printf("4! = %d \n", Factorial(4));
+	printf("9! = %d \n", Factorial(9));
+	return 0;
 	Recursive(3);
 	return 0;
 }
+
+// 재귀함수는 자료구조나 알고리즘의 어려운 문제를 단순화 하는데 사용되는 중요한 무기이다. 
