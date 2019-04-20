d15ae002979fd5237dd45311b9f378363539f503
youjisang <jisangman89@gmail.com>
2019-04-20 20:06:38 +0900

    ArrayList DS

---
+++ C_datastructure/Project6/Project6/PointListMain.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Point.h"
+
+int main(void)
+{
+	List list;
+	Point compPos;
+	Point * ppos;
+
+	ListInit(&list);
+
+	/*** 4개의 데이터 저장 ***/
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 2, 1);
+	LInsert(&list, ppos);
+
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 2, 2);
+	LInsert(&list, ppos);
+
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 3, 1);
+	LInsert(&list, ppos);
+
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 3, 2);
+	LInsert(&list, ppos);
+
+	/*** 저장된 데이터의 출력 ***/
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &ppos))
+	{
+		ShowPointPos(ppos);
+
+		while (LNext(&list, &ppos))
+			ShowPointPos(ppos);
+	}
+	printf("\n");
+
+	/*** xpos가 2인 모든 데이터 삭제 ***/
+	compPos.xpos = 2;
+	compPos.ypos = 0;
+
+	if (LFirst(&list, &ppos))
+	{
+		if (PointComp(ppos, &compPos) == 1)
+		{
+			ppos = LRemove(&list);
+			free(ppos);
+		}
+
+		while (LNext(&list, &ppos))
+		{
+			if (PointComp(ppos, &compPos) == 1)
+			{
+				ppos = LRemove(&list);
+				free(ppos);
+			}
+		}
+	}
+
+	/*** 삭제 후 남은 데이터 전체 출력 ***/
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &ppos))
+	{
+		ShowPointPos(ppos);
+
+		while (LNext(&list, &ppos))
+			ShowPointPos(ppos);
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
+
+/*
+배열 기반 리스트의 단점
+- 배열의 길이가 초기에 결정되어야 한다. 변경이 불가능하다.
+- 삭제의 과정에서 데이터의 이동(복사)가 매우 번번히 일어난다.
+
+배열 기반 리스트의 장점
+- 데이터의 참조가 쉽다. 인덱스 값을 기준으로 어디든 한 번에 참조가 가능하다.
+*/
