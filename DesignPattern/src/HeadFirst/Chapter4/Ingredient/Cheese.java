e98427013862cfa581c89a9cb426ee02c4e4381a
youjisang <jisangman89@gmail.com>
2019-12-31 01:37:43 +0900

    more specific Sample ~ing

---
+++ DesignPattern/src/HeadFirst/Chapter4/Ingredient/Cheese.java
@@ -0,0 +1,4 @@
+package HeadFirst.Chapter4.Ingredient;
+
+public class Cheese {
+}
---
+++ DesignPattern/src/HeadFirst/Chapter4/PizzaIngredientFactory.java
@@ -0,0 +1,12 @@
+package HeadFirst.Chapter4;
+
+import HeadFirst.Chapter4.Ingredient.*;
+
+public interface PizzaIngredientFactory {
+    public Dough createDough();
+    public Sauce createSauce();
+    public Cheese createCheese();
+    public Veggies[] createVeggies();
+    public Pepperoni createPepperoni();
+    public Clams createClam();
+}
